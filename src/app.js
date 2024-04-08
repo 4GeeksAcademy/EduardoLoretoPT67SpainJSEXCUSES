689a4a68b369f76f27ba8f560f4d9212a74c99b9
EduardoLoreto <138321528+EduardoLoreto@users.noreply.github.com>
2024-04-08 12:55:48 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
-/* eslint-disable */
 import "bootstrap";
+
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
+
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuseGenerator();
+  });
+};
+
+let excuseGenerator = () => {
+  let pronoun = ["¡El(la)", "¡Un(a)"];
+  let subject = ["Adulto", "Mascota", "Maestra", "Señor(a)"];
+  let action = ["se le cayeron", "se comio", "asigno", "compró"];
+  let possetion = [
+    "en las escaleras",
+    "Vehiculo",
+    "las Tareas",
+    "un Refresco",
+    "las llaves"
+  ];
+  let where = ["del colegio", "en la casa", "en la clase", "en la tienda"];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subIndx = Math.floor(Math.random() * subject.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let possIndx = Math.floor(Math.random() * possetion.length);
+  let whereIndx = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    possetion[possIndx] +
+    " " +
+    where[whereIndx]
+  );
 };
